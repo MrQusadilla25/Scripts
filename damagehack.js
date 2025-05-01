8a0b447caff871c7cde483e3315ae4c595520afd
MrQusadilla25 <ikertyph@gmail.com>
2025-05-01 15:01:47 -0400

    Update damagehack.js

--- damagehack.js
+++ damagehack.js
@@ -1,67 +1,70 @@
-(function (_0x4f2cd4, _0x3ea2bf) {
-  var _0x151c40 = _0x4f2cd4();
-  while (true) {
-    try {
-      var _0xec97b6 = parseInt(_0x45e9(246, 0x58)) / 1 + parseInt(_0x45e9(252, -0x19)) / 2 + parseInt(_0x45e9(240, 0x64)) / 3 + parseInt(_0x45e9(316, 0x19c)) / 4 + parseInt(_0x45e9(295, 0x178)) / 5 + -parseInt(_0x45e9(321, 0x194)) / 6 + parseInt(_0x45e9(181, -0x53)) / 7 * (-parseInt(_0x45e9(205, -0x4e)) / 8);
-      if (_0xec97b6 === _0x3ea2bf) {
-        break;
-      } else {
-        _0x151c40.push(_0x151c40.shift());
-      }
-    } catch (_0x1d154b) {
-      _0x151c40.push(_0x151c40.shift());
+var _0x15d043 = (function () {
+  var _0x1831e6 = true
+  return function (_0x4185c6, _0x242eac) {
+    var _0x5bcf4a = _0x1831e6
+      ? function () {
+          if (_0x242eac) {
+            var _0x7134c3 = _0x242eac.apply(_0x4185c6, arguments)
+            return (_0x242eac = null), _0x7134c3
+          }
+        }
+      : function () {}
+    return (_0x1831e6 = false), _0x5bcf4a
+  }
+})()
+var _0x5421d2 = _0x15d043(this, function () {
+  return _0x5421d2
+    .toString()
+    .search('(((.+)+)+)+$')
+    .toString()
+    .constructor(_0x5421d2)
+    .search('(((.+)+)+)+$')
+})
+_0x5421d2()
+var _0x3aba75 = (function () {
+    var _0x39dc1c = true
+    return function (_0x22120d, _0x2a6c9f) {
+      var _0x438898 = _0x39dc1c
+        ? function () {
+            if (_0x2a6c9f) {
+              var _0x4d2d24 = _0x2a6c9f.apply(_0x22120d, arguments)
+              return (_0x2a6c9f = null), _0x4d2d24
+            }
+          }
+        : function () {}
+      return (_0x39dc1c = false), _0x438898
     }
-  }
-})(_0x2bd4, 142473);
-function _0x572b03(_0x13851b, _0x396324, _0x590a39, _0x486d54, _0x3d1bad) {
-  return _0x45e9(_0x486d54 - 0x2a, _0x590a39);
-}
-function _0x2bd4() {
-  var _0x540db8 = ['sourc', 'pWeHV', 'SSDUb', '(((.+', 'ctor(', '304666URJjhX', 'ySJKU', '5|1|0', 'NZbmj', 'bJkCm', 'OnuYV', 'HOpQA', 'BXfzf', 'IjFbz', 'CdKRc', 'apply', 'SsKVi', "\"retu", 'trace', 'split', "is\")(", 'cNAMO', 'JumUh', 'tion', 'Bwspl', 'bQhKt', 'YyOiU', 'PKnFs', 'TYEvi', 'aHKEa', 'bind', 'AiBBe', '|2|3', 'rzx)', 'user', '3|2|1', 'UodEw', 'iers', '1|4|0', 'KEJmk', 'DzEhg', 'QmrYA', 'retur', 'prqFz', 'eUqKt', 'ilndM', 'eSyhe', ')+)+)', '881665Ledqjo', 'toStr', 'bWBkp', '|0|5|', 'UGcLZ', 'OkNLi', 'lengt', 'eRJJU', 'nvIYt', "n() ", 'modif', 'SfXQM', 'MNSoj', 'hpsXF', 'IremU', 'bOxMO', 'ent', 'bdKfT', 'damag', 'hEOhq', 'info', '670392dZIcXQ', '__pro', 'tQDJO', 'const', 'JwCNj', '692610MCzEvI', 'log', 'nQwra', 'BiACt', 'MoNfh', "n (fu", 'UYPQn', 'rZnWM', 'imwIP', 'FeytA', 'Yjdke', '{}.co', 'zKgXf', 'kVnqR', 'ARDZk', 'WdGnX', 'XWlTL', 'QkuOo', 'EKqps', '393743zFemQr', 'lPepz', 'GZRHV', "rn th", 'KaGtN', 'oHwPU', 'nstru', 'KRBDL', 'table', 'vzjHf', 'ERFce', 'game', '0|5|3', 'jufPT', '|4|3|', 'zSIVs', '_curr', 'UFvWg', 'XteCZ', 'PimOL', 'VKHNP', 'BwhVe', 'WkPjC', '_stat', '64LeTqAA', 'MvSjw', 'XeJPC', 'error', 'bvMEI', 'proto', 'NRplC', 'lebOc', 'ructo', 'KCXgT', 'DpcST', 'to__', 'JGvGb', 'sEeWD', 'ndeVU', "Made ", 'BxURy', 'KQQzX', 'XwYQQ', 'dRYED', 'ing', "zyx (", 'ldGhO', 'ukQqc', 'sTljl', '|1|2|', 'mioWh', 'nctio', 'BgDBe', 'KTZuO', 'Ldogs', 'conso', 'warn', "by rx", 'oXcjs', '427311bmmRwY', 'itnVP', 'type', 'searc', 'kVYcs', 'excep', '69199xqVIDj'];
-  _0x2bd4 = function () {
-    return _0x540db8;
-  };
-  return _0x2bd4();
-}
-function _0x2b3feb(_0x568218, _0x9d7d7e, _0x21c446, _0x450609, _0xfcc7e1) {
-  return _0x45e9(_0x450609 - 0xab, _0x21c446);
-}
-var _0x37851f = _0x3aba75(this, function () {
-  var _0x45a19e;
-  try {
-    var _0x524b86 = Function("return (function() {}.constructor(\"return this\")( ));");
-    _0x45a19e = _0x524b86();
-  } catch (_0xf38c2a) {
-    _0x45a19e = window;
-  }
-  var _0x2bcfb7 = _0x45a19e.console = _0x45a19e.console || {};
-  var _0x793ac2 = ["log", "warn", "info", "error", "exception", "table", "trace"];
-  for (var _0x49d04c = 0; _0x49d04c < _0x793ac2.length; _0x49d04c++) {
-    var _0x14cedd = _0x3aba75.constructor.prototype.bind(_0x3aba75);
-    var _0x5837c0 = _0x793ac2[_0x49d04c];
-    var _0xc23096 = _0x2bcfb7[_0x5837c0] || _0x14cedd;
-    _0x14cedd.__proto__ = _0x3aba75.bind(_0x3aba75);
-    _0x14cedd.toString = _0xc23096.toString.bind(_0xc23096);
-    _0x2bcfb7[_0x5837c0] = _0x14cedd;
-  }
-});
-function _0x45e9(_0x32e354, _0x5421d2) {
-  var _0x15d043 = _0x2bd4();
-  _0x45e9 = function (_0x2bd4b2, _0x45e978) {
-    _0x2bd4b2 = _0x2bd4b2 - 168;
-    var _0x145ce3 = _0x15d043[_0x2bd4b2];
-    return _0x145ce3;
-  };
-  return _0x45e9(_0x32e354, _0x5421d2);
-}
-function _0x58ce1e(_0x82a15c, _0xc2ec88, _0x204071, _0x4d5b32, _0x3c9c21) {
-  return _0x45e9(_0x204071 - 0x12d, _0x82a15c);
-}
-alert("Made by rxzyx (rzx)");
-function _0x415d4b(_0x30df86, _0x5d20a4, _0x180de3, _0xac7d14, _0x24e8f9) {
-  return _0x45e9(_0x24e8f9 - 0x3b0, _0x180de3);
-}
-function _0x12399e(_0x3e5445, _0x3e8612, _0x26aff3, _0x10b1b2, _0x20f592) {
-  return _0x45e9(_0x26aff3 + 0x2b1, _0x20f592);
-}
-Boot.prototype.game._state._current.user.source.modifiers.damage = Infinity;
+  })(),
+  _0x37851f = _0x3aba75(this, function () {
+    var _0x45a19e
+    try {
+      var _0x524b86 = Function(
+        'return (function() {}.constructor("return this")( ));'
+      )
+      _0x45a19e = _0x524b86()
+    } catch (_0xf38c2a) {
+      _0x45a19e = window
+    }
+    var _0x2bcfb7 = (_0x45a19e.console = _0x45a19e.console || {}),
+      _0x793ac2 = [
+        'log',
+        'warn',
+        'info',
+        'error',
+        'exception',
+        'table',
+        'trace',
+      ]
+    for (var _0x49d04c = 0; _0x49d04c < _0x793ac2.length; _0x49d04c++) {
+      var _0x14cedd = _0x3aba75.constructor.prototype.bind(_0x3aba75)
+      var _0x5837c0 = _0x793ac2[_0x49d04c]
+      var _0xc23096 = _0x2bcfb7[_0x5837c0] || _0x14cedd
+      _0x14cedd['__proto__'] = _0x3aba75.bind(_0x3aba75)
+      _0x14cedd.toString = _0xc23096.toString.bind(_0xc23096)
+      _0x2bcfb7[_0x5837c0] = _0x14cedd
+    }
+  })
+_0x37851f()
+alert('MOD SUCCESSFULLY INJECTED!')
+Boot.prototype.game['_state']['_current'].user.source.modifiers.damage =
+  Infinity
